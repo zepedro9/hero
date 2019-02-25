1fdb0dca0dd34b70ff794d7beb587e4dc29980b2
NepsName <nepsname@gmail.com>
2019-02-25 15:52:04 +0100

    Section 7 finished

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
+import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.screen.TerminalScreen;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+
+import java.io.IOException;
 
 public class Application {
+    public static void main(String[] args) {
+        Game game = new Game();
+        try {
+            game.run();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,4 +1,81 @@
-package PACKAGE_NAME;
+import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.input.KeyType;
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.screen.TerminalScreen;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+
+import java.io.IOException;
 
 public class Game {
+    Screen screen;
+
+    private int x = 10;
+    private int y = 10;
+
+    public Game() {
+        try {
+            Terminal terminal = new DefaultTerminalFactory().createTerminal();
+            screen = new TerminalScreen(terminal);
+
+            screen.setCursorPosition(null);   // we don't need a cursor
+            screen.startScreen();             // screens must be started
+            screen.doResizeIfNecessary();     // resize screen if necessary
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void draw() throws IOException {
+        screen.clear();
+        screen.setCharacter(x, y, new TextCharacter('X'));
+        screen.refresh();
+    }
+
+    public void run() throws IOException {
+        while(true) {
+            draw();
+            KeyStroke key = screen.readInput();
+            if (key.getKeyType().equals(KeyType.EOF)) break;
+            processKey(key);
+        }
+    }
+
+    private void processKey(KeyStroke key) throws IOException {
+        switch(key.getKeyType()) {
+            case ArrowUp:
+                y--;
+                break;
+            case ArrowDown:
+                y++;
+                break;
+            case ArrowLeft:
+                x--;
+                break;
+            case ArrowRight:
+                x++;
+                break;
+            case Character:
+                switch(key.getCharacter()) {
+                    case 'w':
+                        y--;
+                        break;
+                    case 's':
+                        y++;
+                        break;
+                    case 'a':
+                        x--;
+                        break;
+                    case 'd':
+                        x++;
+                        break;
+                    case 'q':
+                        screen.close();
+                        break;
+                }
+                break;
+        }
+    }
 }
